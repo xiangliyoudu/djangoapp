c8a9598e3371329a7fe45f3386029bd9e168483d
xiangliyoudu <wangqiang041045@163.com>
2018-11-21 09:52:34 +0800

    完善appinfo列表信息：所属平台、所属分类、状态、版本； 审核按钮js事件实现

--- djangoapp/backend_app/views.py
+++ djangoapp/backend_app/views.py
@@ -23,13 +23,46 @@ def listApp(req):
     # 查询所有 appinfo
     appInfoList = models.AppInfo.objects.all()
 
+    # AppInfoList关联查询
+    for appInfo in appInfoList:
+        # 查询数据字典，获取平台名称
+        valueId = appInfo.flatformid
+        dataDictionary = models.DataDictionary.objects.filter(typecode='APP_FLATFORM').get(valueid=valueId)
+        appInfo.flatformid = dataDictionary
+        # 查询一级分类，获取分类名称
+        level1Id = appInfo.categorylevel1
+        appCategory1 = models.AppCategory.objects.get(id=level1Id)
+        appInfo.categorylevel1 = appCategory1
+        # 查询二级分类，获取分类名称
+        level2Id = appInfo.categorylevel2
+        appCategory2 = models.AppCategory.objects.get(id=level2Id)
+        appInfo.categorylevel2 = appCategory2
+        # 查询三级分类，获取分类名称
+        level3Id = appInfo.categorylevel3
+        appCategory3 = models.AppCategory.objects.get(id=level3Id)
+        appInfo.categorylevel3 = appCategory3
+        # 查询数据字典，获取app状态
+        valueId = appInfo.status
+        dataDictionary = models.DataDictionary.objects.filter(typecode='APP_STATUS').get(valueid=valueId)
+        appInfo.status = dataDictionary
+        # 查询app版本，获取最新版本信息
+        versionId = appInfo.versionid
+        try:
+            appVersion = models.AppVersion.objects.get(id=versionId)
+        except Exception as e:
+            appVersion = '无'
+        if appVersion:
+            appInfo.versionid = appVersion
+
+
+
     cx['appInfoList'] = appInfoList
     cx['flatFormList'] = flatFormList
     cx['categoryLevel1List'] = categoryLevel1List
 
     return render(req, 'appList.html', context=cx)
 
-# 二三级分类ajax查询
+# 二 三级分类ajax查询
 def categorylevellistJson(req):
     # 根据一级、二级分类查询二、三级分类记录
     pid = req.GET.get('pid')

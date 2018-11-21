515135f11be838dd5e9bf0b43d70351801bbead1
xiangliyoudu <wangqiang041045@163.com>
2018-11-21 18:39:12 +0800

    app审查页面，返回按钮实现

--- djangoapp/static/localjs/appcheck.js
+++ djangoapp/static/localjs/appcheck.js
@@ -1,3 +1,14 @@
 $("#back").on("click",function(){
-	window.location.href = "/backend/app/list";
+	window.location.href = "../";
+	//window.history.back(-1);
 });
+
+function getRootPath() {
+	var curWwwPath = window.document.location.href;
+	var pathName = window.document.location.pathname;
+	var pos = curWwwPath.indexOf(pathName);
+	var localhostPaht = curWwwPath.substring(0, pos);
+	var projectName = pathName
+			.substring(0, pathName.substr(1).indexOf('/') + 1);
+	return (localhostPaht + projectName);
+}
